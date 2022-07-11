f7bdcbf5b8e2e3f40a91a78cb8cc3a3ab20ba83e
Tawashiy <97021226+Tawashiy@users.noreply.github.com>
2022-07-11 22:38:29 +0900

    「33.Organismの作成2」まで

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 
 import "./styles.css";
+
+const user = {
+  name: "わんこ",
+  image: "https://source.unsplash.com/Sg3XwuEpybU",
+  email: "aaa@aaaa.aa",
+  phone: "090-0000-0000",
+  company: {
+    name: "わんこ株式会社"
+  },
+  website: "htttp://asasas"
+};
 
 export const App = () => {
   return (
@@ -11,6 +23,7 @@ export const App = () => {
       <SecondaryButton>ぼたん</SecondaryButton>
       　 <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 };
